ee36b985faa42465e0ca9270f061e0e10d065530
Tom Hawkins <tomahawkins@gmail.com>
2014-03-04 11:57:55 -0600

    Cleaning the example more, still has problems with mutual-recursion.

--- sandbox/factorial.lisp
+++ sandbox/factorial.lisp
@@ -1,32 +1,27 @@
 (mutual-recursion
-
   ; Pop the continuation name off the stack and call it with the return value, else return the return value.
   (defun call-cont (stack retval)
-    ;(declare (xargs :measure (len stack)))
-    (if (equal (car stack) "factorial-cont")
+    (declare (xargs :measure (len stack)))
+    (if (and (consp stack) (equal (car stack) "factorial-cont"))
       (factorial-cont (cdr stack) retval)
       retval
     )
   )
 
-  ; The continuation of the factorial function.  Restores the input argument 'n' and multiplies it with the result of the recursive call.
+  ; The continuation of the factorial function.  Restores the input argument 'n' from the stack
+  ; and multiplies it with the result of the recursive call.
   (defun factorial-cont (stack retval)
-    ;(declare (xargs :measure (len stack)))
-    (let*
-      ( (n     (car stack))  ; Restore the input argument.
-        (stack (cdr stack))  ; Update the stack.
-      )
-      (call-cont stack (* n retval))
-    )
+    (declare (xargs :measure (len stack)))
+    (call-cont (cdr stack) (* (car stack) retval))
   )
+)
 
-  ; The factorial function takes the stack and the number to compute the factorial.
-  (defun factorial (stack n)
-    ;(declare (xargs :measure n))
-    (if (> n 1)
-      (factorial (cons "factorial-cont" (cons n stack)) (- n 1))  ; On the recursive call, push 'n' and continuation name onto the stack.
-      (call-cont stack n)
-    )
+; The factorial function takes the stack and the number to compute the factorial.
+(defun factorial (stack n)
+  (if (and (integerp n) (> n 1))
+    ; On the recursive call, push 'n' and the continuation name onto the stack.
+    (factorial (cons "factorial-cont" (cons n stack)) (- n 1))
+    (call-cont stack n)
   )
 )
 
