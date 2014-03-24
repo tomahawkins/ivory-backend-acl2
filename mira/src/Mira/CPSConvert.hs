03033f6e64cd2b30d6b01684e0caca3457a55c19
Tom Hawkins <tomahawkins@gmail.com>
2014-03-24 13:00:36 -0500

    Loop termination working with the help of compiler generated measures.

--- mira/src/Mira/CPSConvert.hs
+++ mira/src/Mira/CPSConvert.hs
@@ -71,7 +71,9 @@ cpsStmts a cont = case a of
         fun  <- genVar
         test <- genVar
         one  <- genVar
-        addProc fun args (Just $ E.Intrinsic Sub [E.Var i, E.Var to]) $ Let test (Intrinsic (if incr then Le else Ge) [i, to]) $ If test (replaceCont (f fun i args one) body) cont
+        addProc fun args
+          (Just $ E.Intrinsic Add [E.Literal $ LitInteger 1, E.Intrinsic Sub [E.Var i, E.Var to]]) $
+          Let test (Intrinsic (if incr then Le else Ge) [i, to]) $ If test (replaceCont (f fun i args one) body) cont
         return $ Call fun (init : to : args') $ Just cont
         where
         -- Replace Halt with recursive call.
